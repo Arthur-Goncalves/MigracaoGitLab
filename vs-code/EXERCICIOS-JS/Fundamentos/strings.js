3f9e152eeca67b2ba259ad1f4e91071a5dabc1e9
ArthurGonçalves <arthurgon2102@gmail.com>
2020-03-24 21:29:03 -0300

    Aulas sobre String

--- vs-code/EXERCICIOS-JS/Fundamentos/strings.js
+++ vs-code/EXERCICIOS-JS/Fundamentos/strings.js
@@ -7,3 +7,9 @@ console.log(escola.indexOf('3'))
 
 console.log(escola.substring(1)) // foi do indice 1 até o final.
 console.log(escola.substring(0, 3)) // foi do indice 0 até a o terceiro dígito.
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana,Maria,Pedro'.split(',')) //estrutura do array
+
