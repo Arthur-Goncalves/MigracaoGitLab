63a8503d03a19dc9639dbab595d1656f5e7ad06c
ArthurGonçalves <arthurgon2102@gmail.com>
2020-05-12 00:47:15 -0300

    Estudo Java

--- eclipse-workspace/exerc_condicional/src/exerc_condicional/Exerc05.java
+++ eclipse-workspace/exerc_condicional/src/exerc_condicional/Exerc05.java
@@ -5,34 +5,30 @@ import java.util.Scanner;
 public class Exerc05 {
 
 	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner (System.in);
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		int codigo = sc.nextInt();
 		int quantidade = sc.nextInt();
-		
+
 		double total;
 		if (codigo == 1) {
 			total = quantidade * 4.0;
-		}
-		else if (codigo == 2) {
+		} else if (codigo == 2) {
 			total = quantidade * 4.5;
-		}
-		else if (codigo == 3) {
+		} else if (codigo == 3) {
 			total = quantidade * 5.0;
-		}
-		else if (codigo == 4) {
+		} else if (codigo == 4) {
 			total = quantidade * 2.0;
-		}
-		else {
+		} else {
 			total = quantidade * 1.5;
 		}
-		
-		//TESTE DE COMMIT NO GITLAB
-		
+
+		// TESTE DE COMMIT NO GITLAB
+
 		System.out.printf("Total: R$ %.2f%n", total);
-		
-		sc.close();		
+
+		sc.close();
 	}
 
 }
